b771e02d9f7b8799f1d3cf25eeff596a77c8a434
iqmeta <microsoft@iqmeta.com>
2025-12-23 05:55:51 +0100

    Bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DxTextBoxModule, DxValidatorModule } from 'devextreme-angular';
+import { ValidationCallbackData } from 'devextreme/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DxTextBoxModule, DxValidatorModule, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('bug');
+
+  email: string = 'test@test.test';
+
+  //WORKED YEARS NOW (still deployed) IN PROJECT BEFORE - BUT NOW COMPILATION ERROR => AND RUNTIME ERROR SAYS: validationCallback of an asynchronous rule should return a jQuery or a native promise
+  // async emailValidation(params: ValidationCallbackData): Promise<boolean> {
+  //   return new Promise((resolve) => {
+  //     setTimeout(() => { resolve(true); }, 2000);
+  //   });
+  // }
+
+  //COMPILES - BUT RUNTIME ERROR WHEN VALIDATION OCCURS JUST TRY CHANGE EMAIL SEE CONSOLE
+  emailValidation(params: ValidationCallbackData): boolean {    
+    return true;
+  }
+
 }
